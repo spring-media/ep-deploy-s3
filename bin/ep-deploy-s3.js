9205cee5434c3ecbc505e32a29309fd3f7596b70
Torsten Hain <torsten.hain@spring-media.de>
2018-11-09 16:22:06 +0100

    add s3 put params and removed old dependency

--- bin/ep-deploy-s3.js
+++ bin/ep-deploy-s3.js
@@ -1,39 +1,38 @@
 #!/usr/bin/env node
 
-'use strict';
+'use strict'
 
-const spawn = require('cross-spawn');
-const script = process.argv[2];
-const args = process.argv.slice(3);
+const spawn = require('cross-spawn')
+const script = process.argv[2]
+const args = process.argv.slice(3)
 
 switch (script) {
-    case 'environments':
-    case 'deploy': {
-        const result = spawn.sync(
-            'node',
-            [require.resolve(`../dist/${script}`)].concat(args),
-            { stdio: 'inherit' }
-        );
-        if (result.signal) {
-            if (result.signal === 'SIGKILL') {
-                console.log(
-                    'The process exited too early. ' +
-                    'This probably means the system ran out of memory or someone called ' +
-                    '`kill -9` on the process.'
-                );
-            } else if (result.signal === 'SIGTERM') {
-                console.log(
-                    'The process exited too early. ' +
-                    'Someone might have called `kill` or `killall`, or the system could ' +
-                    'be shutting down.'
-                );
-            }
-            process.exit(1);
-        }
-        process.exit(result.status);
-        break;
+  case 'environments':
+  case 'deploy': {
+    const result = spawn.sync(
+      'node',
+      [require.resolve(`../dist/${script}`)].concat(args),
+      { stdio: 'inherit' }
+    )
+    if (result.signal) {
+      if (result.signal === 'SIGKILL') {
+        console.log(
+          'The process exited too early. ' +
+            'This probably means the system ran out of memory or someone called ' +
+            '`kill -9` on the process.'
+        )
+      } else if (result.signal === 'SIGTERM') {
+        console.log(
+          'The process exited too early. ' +
+            'Someone might have called `kill` or `killall`, or the system could ' +
+            'be shutting down.'
+        )
+      }
+      process.exit(1)
     }
-    default:
-        console.log('Unknown command "' + script + '".');
-        break;
+    process.exit(result.status)
+  }
+  default:
+    console.log('Unknown command "' + script + '".')
+    break
 }
--- dist/environment.js
+++ dist/environment.js
@@ -120,20 +120,20 @@ const path = __webpack_require__(1);
  **************************************** */
 
 if (argv.dir) {
-    console.log(chalk.dim('\nProcessing files in ' + path.resolve(argv.dir) + '\n'));
-    const files = glob.readdirSync(argv.dir + '/*.sample.js');
-    files.forEach(replaceEnvVars);
-    console.log(chalk.green('Finished successfully') + '\n');
+  console.log(chalk.dim('\nProcessing files in ' + path.resolve(argv.dir) + '\n'));
+  const files = glob.readdirSync(argv.dir + '/*.sample.js');
+  files.forEach(replaceEnvVars);
+  console.log(chalk.green('Finished successfully') + '\n');
 }
 
 if (argv.file) {
-    if (!/.sample.js$/.test(argv.file)) {
-        console.log(chalk.red('Error:') + ' File must end with .sample.js');
-        process.exit(1);
-    }
-    console.log(chalk.dim('\nProcessing file in ' + path.resolve(argv.file) + '\n'));
-    replaceEnvVars(argv.file);
-    console.log(chalk.green('Finished successfully') + '\n');
+  if (!/.sample.js$/.test(argv.file)) {
+    console.log(chalk.red('Error:') + ' File must end with .sample.js');
+    process.exit(1);
+  }
+  console.log(chalk.dim('\nProcessing file in ' + path.resolve(argv.file) + '\n'));
+  replaceEnvVars(argv.file);
+  console.log(chalk.green('Finished successfully') + '\n');
 }
 
 /* ****************************************
@@ -143,30 +143,29 @@ if (argv.file) {
  **************************************** */
 
 function replaceEnvVars(inputFile) {
+  const outputFile = inputFile.replace(/.sample.js$/, '.js');
+  const inputContents = fs.readFileSync(inputFile, 'utf8');
 
-    const outputFile = inputFile.replace(/.sample.js$/, '.js');
-    const inputContents = fs.readFileSync(inputFile, 'utf8');
+  console.log('📄  ' + path.basename(inputFile) + ' -> ' + path.basename(outputFile));
 
-    console.log('📄  ' + path.basename(inputFile) + ' -> ' + path.basename(outputFile));
+  const outputContents = inputContents.replace(/<\s*(\S+?)\s*>/g, function (match, envVarName) {
+    const envVar = process.env[envVarName];
+    if (envVar === undefined && argv['skip-unknown']) {
+      console.log('   ' + chalk.red('x') + chalk.dim(' Skipping ' + envVarName + ' (not defined in process.env)'));
+      return match;
+    } else if (envVar === undefined) {
+      console.log('   ' + chalk.red('x') + ' ' + envVarName + ' not defined in process.env\n');
+      process.exit(1);
+    } else {
+      console.log('   ' + chalk.green('✓') + chalk.dim(' Replacing ' + envVarName));
+    }
 
-    const outputContents = inputContents.replace(/<\s*(\S+?)\s*>/g, function (match, envVarName) {
-        const envVar = process.env[envVarName];
-        if (envVar === undefined && argv['skip-unknown']) {
-            console.log('   ' + chalk.red('x') + chalk.dim(' Skipping ' + envVarName + ' (not defined in process.env)'));
-            return match;
-        } else if (envVar === undefined) {
-            console.log('   ' + chalk.red('x') + ' ' + envVarName + ' not defined in process.env\n');
-            process.exit(1);
-        } else {
-            console.log('   ' + chalk.green('✓') + chalk.dim(' Replacing ' + envVarName));
-        }
+    return envVar;
+  });
 
-        return envVar;
-    });
+  fs.writeFileSync(outputFile, outputContents);
 
-    fs.writeFileSync(outputFile, outputContents);
-
-    console.log('');
+  console.log('');
 }
 
 /***/ }),
