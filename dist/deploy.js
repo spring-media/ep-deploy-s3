7d133fd331b6519f2a7b628f1e63ed349db5280a
Torsten Hain <tdt.hain@gmx.de>
2018-11-12 11:33:13 +0100

    Add put params (#2)
    * add s3 put params and removed old dependency
    * dependency upgrade

--- dist/deploy.js
+++ dist/deploy.js
@@ -111,14 +111,14 @@ module.exports = require("yargs");
  * @param {string} remoteDir The path in the bucket to sync to
  */
 let sync = (() => {
-    var _ref = _asyncToGenerator(function* (localDir, bucket, remoteDir) {
-        yield removeFromS3(bucket, remoteDir);
-        yield uploadToS3(localDir, bucket, remoteDir);
-    });
+  var _ref = _asyncToGenerator(function* (localDir, bucket, remoteDir) {
+    yield removeFromS3(bucket, remoteDir);
+    yield uploadToS3(localDir, bucket, remoteDir);
+  });
 
-    return function sync(_x, _x2, _x3) {
-        return _ref.apply(this, arguments);
-    };
+  return function sync(_x, _x2, _x3) {
+    return _ref.apply(this, arguments);
+  };
 })();
 
 /**
@@ -130,33 +130,32 @@ let sync = (() => {
 
 
 let removeFromS3 = (() => {
-    var _ref2 = _asyncToGenerator(function* (bucket, remoteDir) {
+  var _ref2 = _asyncToGenerator(function* (bucket, remoteDir) {
+    console.log('\nDeleting remote directory ' + bucket + '/' + remoteDir);
 
-        console.log('\nDeleting remote directory ' + bucket + '/' + remoteDir);
-
-        const awsS3Client = new _awsSdk2.default.S3({
-            region: 'eu-central-1',
-            signatureVersion: 'v4'
-        });
-
-        const items = remoteDir ? [{ Key: remoteDir }] : (yield awsS3Client.listObjects({ Bucket: bucket }).promise()).Contents;
-        const deleteParamsForItems = items.map(function (item) {
-            return { Bucket: bucket, Key: item.Key };
-        });
-
-        try {
-            for (const itemDeleteParams of deleteParamsForItems) {
-                yield awsS3Client.deleteObject(itemDeleteParams).promise();
-                process.stdout.write('.');
-            }
-        } catch (err) {
-            throw new Error('Unable remove files:', err.message + '\n');
-        }
+    const awsS3Client = new _awsSdk2.default.S3({
+      region: 'eu-central-1',
+      signatureVersion: 'v4'
     });
 
-    return function removeFromS3(_x4, _x5) {
-        return _ref2.apply(this, arguments);
-    };
+    const items = remoteDir ? [{ Key: remoteDir }] : (yield awsS3Client.listObjects({ Bucket: bucket }).promise()).Contents;
+    const deleteParamsForItems = items.map(function (item) {
+      return { Bucket: bucket, Key: item.Key };
+    });
+
+    try {
+      for (const itemDeleteParams of deleteParamsForItems) {
+        yield awsS3Client.deleteObject(itemDeleteParams).promise();
+        process.stdout.write('.');
+      }
+    } catch (err) {
+      throw new Error('Unable remove files:', err.message + '\n');
+    }
+  });
+
+  return function removeFromS3(_x4, _x5) {
+    return _ref2.apply(this, arguments);
+  };
 })();
 
 /**
@@ -169,26 +168,25 @@ let removeFromS3 = (() => {
 
 
 let uploadToS3 = (() => {
-    var _ref3 = _asyncToGenerator(function* (localDir, bucket, remoteDir = '') {
+  var _ref3 = _asyncToGenerator(function* (localDir, bucket, remoteDir = '') {
+    console.log('\nUploading local directory to ' + bucket + '/' + remoteDir);
 
-        console.log('\nUploading local directory to ' + bucket + '/' + remoteDir);
-
-        const awsS3Client = new _awsSdk2.default.S3({
-            region: 'eu-central-1',
-            signatureVersion: 'v4'
-        });
-
-        try {
-            yield copyFolderContentsToBucket(awsS3Client, bucket, remoteDir, localDir);
-            console.log('\n' + _chalk2.default.green('Done!'));
-        } catch (err) {
-            throw new Error('Unable upload files:', err.message + '\n');
-        }
+    const awsS3Client = new _awsSdk2.default.S3({
+      region: 'eu-central-1',
+      signatureVersion: 'v4'
     });
 
-    return function uploadToS3(_x6, _x7) {
-        return _ref3.apply(this, arguments);
-    };
+    try {
+      yield copyFolderContentsToBucket(awsS3Client, bucket, remoteDir, localDir);
+      console.log('\n' + _chalk2.default.green('Done!'));
+    } catch (err) {
+      throw new Error('Unable upload files:', err.message + '\n');
+    }
+  });
+
+  return function uploadToS3(_x6, _x7) {
+    return _ref3.apply(this, arguments);
+  };
 })();
 
 /**
@@ -203,31 +201,29 @@ let uploadToS3 = (() => {
 
 
 let copyFolderContentsToBucket = (() => {
-    var _ref4 = _asyncToGenerator(function* (awsS3Client, bucket, prefix, rootFolder, subFolder = '') {
+  var _ref4 = _asyncToGenerator(function* (awsS3Client, bucket, prefix, rootFolder, subFolder = '') {
+    const fileNames = _fs2.default.readdirSync(_path2.default.join(rootFolder, subFolder));
 
-        const fileNames = _fs2.default.readdirSync(_path2.default.join(rootFolder, subFolder));
+    for (const fileName of fileNames) {
+      const pathToFileInSubFolder = _path2.default.join(subFolder, fileName);
+      const pathToFileInRootFolder = _path2.default.join(rootFolder, subFolder, fileName);
+      const fileIsDirectory = _fs2.default.lstatSync(pathToFileInRootFolder).isDirectory();
 
-        for (const fileName of fileNames) {
+      if (fileIsDirectory) {
+        yield copyFolderContentsToBucket(awsS3Client, bucket, prefix, rootFolder, pathToFileInSubFolder);
+      } else {
+        const Bucket = bucket;
+        const Key = prefix ? `${prefix}/${pathToFileInSubFolder}` : pathToFileInSubFolder;
+        const Body = _fs2.default.readFileSync(pathToFileInRootFolder);
+        yield awsS3Client.putObject(Object.assign(config.putParams, { Bucket, Key, Body })).promise();
+        process.stdout.write('.');
+      }
+    }
+  });
 
-            const pathToFileInSubFolder = _path2.default.join(subFolder, fileName);
-            const pathToFileInRootFolder = _path2.default.join(rootFolder, subFolder, fileName);
-            const fileIsDirectory = _fs2.default.lstatSync(pathToFileInRootFolder).isDirectory();
-
-            if (fileIsDirectory) {
-                yield copyFolderContentsToBucket(awsS3Client, bucket, prefix, rootFolder, pathToFileInSubFolder);
-            } else {
-                const Bucket = bucket;
-                const Key = prefix ? `${prefix}/${pathToFileInSubFolder}` : pathToFileInSubFolder;
-                const Body = _fs2.default.readFileSync(pathToFileInRootFolder);
-                yield awsS3Client.putObject({ Bucket, Key, Body }).promise();
-                process.stdout.write('.');
-            }
-        }
-    });
-
-    return function copyFolderContentsToBucket(_x8, _x9, _x10, _x11) {
-        return _ref4.apply(this, arguments);
-    };
+  return function copyFolderContentsToBucket(_x8, _x9, _x10, _x11) {
+    return _ref4.apply(this, arguments);
+  };
 })();
 
 var _fs = __webpack_require__(0);
@@ -265,10 +261,10 @@ console.log(`\n${_chalk2.default.blue('info')} Loading config`);
 let config;
 
 try {
-    config = getConfig();
+  config = getConfig();
 } catch (err) {
-    console.log(`\n${_chalk2.default.red('error')} Failed to load config. Got error: ${err.message}`);
-    process.exit(1);
+  console.log(`\n${_chalk2.default.red('error')} Failed to load config. Got error: ${err.message}`);
+  process.exit(1);
 }
 
 /* ****************************************
@@ -285,11 +281,11 @@ console.log(' ' + _chalk2.default.green('✓') + _chalk2.default.dim(' Bucket Di
 console.log('\nSyncing local dir to s3');
 
 sync(config.localDir, config.bucket, config.remoteDir).then(() => {
-    console.log('\n' + _chalk2.default.green('Sync finished successfully') + '\n');
-    process.exit(0);
+  console.log('\n' + _chalk2.default.green('Sync finished successfully') + '\n');
+  process.exit(0);
 }).catch(err => {
-    console.log(`\n${_chalk2.default.red('error')} Failed to sync. Got error: ${err.message}`);
-    process.exit(1);
+  console.log(`\n${_chalk2.default.red('error')} Failed to sync. Got error: ${err.message}`);
+  process.exit(1);
 });
 
 /* ****************************************
@@ -303,45 +299,49 @@ sync(config.localDir, config.bucket, config.remoteDir).then(() => {
  * @returns {{localDir: string, bucket: string, remoteDir: string}}
  */
 function getConfig() {
+  const configFile = _path2.default.resolve('./ep-deployment.json');
+  const configFileExists = _fs2.default.existsSync(configFile);
 
-    const configFile = _path2.default.resolve('./ep-deployment.json');
-    const configFileExists = _fs2.default.existsSync(configFile);
+  if (configFileExists && !_yargs.argv.stage) {
+    throw new Error('Config file exists but no stage has been set via cli argument. Please specify --stage');
+  } else if (configFileExists) {
+    console.log(' ' + _chalk2.default.green('✓') + _chalk2.default.dim(' Found config file in ' + configFile));
+  } else {
+    console.log(_chalk2.default.blue('info') + _chalk2.default.dim(' No config file found expecting command line arguments'));
+  }
 
-    if (configFileExists && !_yargs.argv.stage) {
-        throw new Error('Config file exists but no stage has been set via cli argument. Please specify --stage');
-    } else if (configFileExists) {
-        console.log(' ' + _chalk2.default.green('✓') + _chalk2.default.dim(' Found config file in ' + configFile));
-    } else {
-        console.log(_chalk2.default.blue('info') + _chalk2.default.dim(' No config file found expecting command line arguments'));
-    }
+  if (_yargs.argv.dir || _yargs.argv.bucket) {
+    console.log(' ' + _chalk2.default.green('✓') + _chalk2.default.dim(' Got command line arguments for dir/bucket'));
+  }
 
-    if (_yargs.argv.dir || _yargs.argv.bucket) {
-        console.log(' ' + _chalk2.default.green('✓') + _chalk2.default.dim(' Got command line arguments for dir/bucket'));
-    }
+  const config = Object.assign({}, readConfigFromJson(configFile), readConfigFromArgv());
 
-    const config = Object.assign({}, readConfigFromJson(configFile), readConfigFromArgv());
+  if (!config.dir) {
+    throw new Error('No directory specified');
+  }
 
-    if (!config.dir) {
-        throw new Error('No directory specified');
-    }
+  if (!config.bucket) {
+    throw new Error('No bucket specified');
+  }
 
-    if (!config.bucket) {
-        throw new Error('No bucket specified');
-    }
+  if (typeof config.putParams !== 'object') {
+    config.putParams = {};
+  }
 
-    const bucketAndPath = config.bucket.split('/');
-    const bucket = bucketAndPath.shift();
-    const remoteDir = bucketAndPath.join('/');
+  const bucketAndPath = config.bucket.split('/');
+  const bucket = bucketAndPath.shift();
+  const remoteDir = bucketAndPath.join('/');
 
-    if (remoteDir === '' && !_yargs.argv['sync-to-bucket-root']) {
-        throw new Error('You are trying to sync to the bucket root (not in a subfolder of the bucket).\n' + 'Please add --sync-to-bucket-root to signal you are absolutely sure you are willing to do this.\n' + 'Warning: All contents of the bucket will be deleted and synced with your local folder');
-    }
+  if (remoteDir === '' && !_yargs.argv['sync-to-bucket-root']) {
+    throw new Error('You are trying to sync to the bucket root (not in a subfolder of the bucket).\n' + 'Please add --sync-to-bucket-root to signal you are absolutely sure you are willing to do this.\n' + 'Warning: All contents of the bucket will be deleted and synced with your local folder');
+  }
 
-    return {
-        localDir: _path2.default.resolve(config.dir),
-        bucket: bucket,
-        remoteDir: remoteDir
-    };
+  return {
+    localDir: _path2.default.resolve(config.dir),
+    bucket,
+    remoteDir,
+    putParams: config.putParams
+  };
 }
 
 /**
@@ -349,15 +349,15 @@ function getConfig() {
  * @returns {{dir: string, bucket: string}|{}}
  */
 function readConfigFromJson(configFile) {
-    if (!_yargs.argv.stage) {
-        throw new Error('No stage specified, please call ep-deploy-s3 with --stage dev or --stage prod');
-    }
-    try {
-        return (0, _jsonfile.readFileSync)(configFile)[_yargs.argv.stage];
-    } catch (e) {
-        console.log(`${_chalk2.default.blue('info')} no config file found`);
-        return {};
-    }
+  if (!_yargs.argv.stage) {
+    throw new Error('No stage specified, please call ep-deploy-s3 with --stage dev or --stage prod');
+  }
+  try {
+    return (0, _jsonfile.readFileSync)(configFile)[_yargs.argv.stage];
+  } catch (e) {
+    console.log(`${_chalk2.default.blue('info')} no config file found`);
+    return {};
+  }
 }
 
 /**
@@ -365,10 +365,11 @@ function readConfigFromJson(configFile) {
  * @returns {{dir: string, bucket: string}}
  */
 function readConfigFromArgv() {
-    const config = {};
-    if (_yargs.argv.dir) config.dir = _yargs.argv.dir;
-    if (_yargs.argv.bucket) config.bucket = _yargs.argv.bucket;
-    return config;
+  const config = {};
+  if (_yargs.argv.dir) config.dir = _yargs.argv.dir;
+  if (_yargs.argv.putParams) config.putParams = JSON.parse(_yargs.argv.putParams);
+  if (_yargs.argv.bucket) config.bucket = _yargs.argv.bucket;
+  return config;
 }
 
 /***/ }),
--- src/environments.js
+++ src/environments.js
@@ -1,8 +1,8 @@
-const fs = require('fs');
-const glob = require('glob-fs')({ gitignore: true });
-const chalk = require('chalk');
-const argv = require('yargs').argv;
-const path = require('path');
+const fs = require('fs')
+const glob = require('glob-fs')({ gitignore: true })
+const chalk = require('chalk')
+const argv = require('yargs').argv
+const path = require('path')
 
 /* ****************************************
  *
@@ -11,22 +11,21 @@ const path = require('path');
  **************************************** */
 
 if (argv.dir) {
-    console.log(chalk.dim('\nProcessing files in ' + path.resolve(argv.dir) + '\n'));
-    const files = glob.readdirSync(argv.dir + '/*.sample.js');
-    files.forEach(replaceEnvVars);
-    console.log(chalk.green('Finished successfully') + '\n');
+  console.log(chalk.dim('\nProcessing files in ' + path.resolve(argv.dir) + '\n'))
+  const files = glob.readdirSync(argv.dir + '/*.sample.js')
+  files.forEach(replaceEnvVars)
+  console.log(chalk.green('Finished successfully') + '\n')
 }
 
 if (argv.file) {
-    if (!/.sample.js$/.test(argv.file)) {
-        console.log(chalk.red('Error:') + ' File must end with .sample.js');
-        process.exit(1);
-    }
-    console.log(chalk.dim('\nProcessing file in ' + path.resolve(argv.file) + '\n'));
-    replaceEnvVars(argv.file);
-    console.log(chalk.green('Finished successfully') + '\n');
+  if (!/.sample.js$/.test(argv.file)) {
+    console.log(chalk.red('Error:') + ' File must end with .sample.js')
+    process.exit(1)
+  }
+  console.log(chalk.dim('\nProcessing file in ' + path.resolve(argv.file) + '\n'))
+  replaceEnvVars(argv.file)
+  console.log(chalk.green('Finished successfully') + '\n')
 }
-
 
 /* ****************************************
  *
@@ -34,30 +33,28 @@ if (argv.file) {
  *
  **************************************** */
 
-function replaceEnvVars(inputFile) {
+function replaceEnvVars (inputFile) {
+  const outputFile = inputFile.replace(/.sample.js$/, '.js')
+  const inputContents = fs.readFileSync(inputFile, 'utf8')
 
-    const outputFile = inputFile.replace(/.sample.js$/, '.js');
-    const inputContents = fs.readFileSync(inputFile, 'utf8');
+  console.log('📄  ' + path.basename(inputFile) + ' -> ' + path.basename(outputFile))
 
-    console.log('📄  ' + path.basename(inputFile) + ' -> ' + path.basename(outputFile));
+  const outputContents = inputContents.replace(/<\s*(\S+?)\s*>/g, function (match, envVarName) {
+    const envVar = process.env[envVarName]
+    if (envVar === undefined && argv['skip-unknown']) {
+      console.log('   ' + chalk.red('x') + chalk.dim(' Skipping ' + envVarName + ' (not defined in process.env)'))
+      return match
+    } else if (envVar === undefined) {
+      console.log('   ' + chalk.red('x') + ' ' + envVarName + ' not defined in process.env\n')
+      process.exit(1)
+    } else {
+      console.log('   ' + chalk.green('✓') + chalk.dim(' Replacing ' + envVarName))
+    }
 
-    const outputContents = inputContents.replace(/<\s*(\S+?)\s*>/g, function (match, envVarName) {
-        const envVar = process.env[envVarName];
-        if (envVar === undefined && argv['skip-unknown']) {
-            console.log('   ' + chalk.red('x') + chalk.dim(' Skipping ' + envVarName + ' (not defined in process.env)'));
-            return match;
-        } else if (envVar === undefined) {
-            console.log('   ' + chalk.red('x') + ' ' + envVarName + ' not defined in process.env\n');
-            process.exit(1);
-        } else {
-            console.log('   ' + chalk.green('✓') + chalk.dim(' Replacing ' + envVarName));
-        }
+    return envVar
+  })
 
-        return envVar;
-    });
+  fs.writeFileSync(outputFile, outputContents)
 
-    fs.writeFileSync(outputFile, outputContents);
-
-    console.log('');
-
+  console.log('')
 }
